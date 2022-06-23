f002fb24bce478962872761d4ec4eff5922e9988
gusgraujo <gusg.raujo@gmail.com>
2022-06-23 10:37:07 -0300

    feature: implement product details page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "85a7676bfc1d8561"; }
+/******/ 		__webpack_require__.h = function() { return "746763a52657f31a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
