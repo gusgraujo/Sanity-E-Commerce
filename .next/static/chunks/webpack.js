acd4baa38e291a67f3140a13a19a8b860eea6d55
gusgraujo <gusg.raujo@gmail.com>
2022-07-05 00:31:42 -0300

    fix: server

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "746763a52657f31a"; }
+/******/ 		__webpack_require__.h = function() { return "57f9cbe39696bf9c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
